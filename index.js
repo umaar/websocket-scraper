24ff725c605c3776e7137a9314e9a2345233a358
Umar Hansa <umar.hansa@gmail.com>
2019-03-13 23:13:15 +0000

    Clean up code

--- index.js
+++ index.js
@@ -1,83 +1,89 @@
-/*
-*	Scrape from WebSocket stream and rotate them to disk
-*/
+const KB_FACTOR = 1024;
+const MB_FACTOR = 1024 * KB_FACTOR;
+const GB_FACTOR = 1024 * MB_FACTOR;
 
-var KB_FACTOR = 1024;
-var MB_FACTOR = 1024 * KB_FACTOR;
-var GB_FACTOR = 1024 * MB_FACTOR;
+const maxSize = Number(MB_FACTOR);
 
-var maxSize = 1 * MB_FACTOR;
+const keep = 2;
+let rotations = 0;
+let count = 0;
 
-var keep = 2;
-var rotations = 0;
-var count = 0;
+const RotatingLog = require('rotating-log');
 
-var RotatingLog = require('rotating-log')
-,   logfile     = 'logs/logs.log'
-,   log         = RotatingLog(logfile, {keep: keep, maxsize: Math.round(maxSize) })
+const logfile = 'logs/logs.log';
+
+const log = RotatingLog(logfile, {keep, maxsize: Math.round(maxSize)});
 
 function checkRotations() {
 	if (rotations >= keep) {
-		console.log('There have been ', rotations, '. Quitting websocket.');
+		console.log('There have been', rotations, '. Quitting websocket.');
 		ws.close();
 	}
 }
 
-log.on('rotated', function () {
-	console.log('The log file was rotated.')
+log.on('rotated', () => {
+	console.log('The log file was rotated.');
 	rotations++;
 	checkRotations();
-})
+});
 
-log.on('error', function (err) {
-	console.error('There was an error: %s', err.message || err)
-})
-//log.write( 'data' )
+log.on('error', err => {
+	console.error('There was an error: %s', err.message || err);
+});
+// Log.write( 'data' )
 
-console.log('Logging to ', logfile, ' max size = ', maxSize);
-console.log('execute "tail -f %s" to watch log. press ctrl-c to exit.', logfile)
-console.log('Starting: ', new Date());
+console.log('Logging to', logfile, 'max size =', maxSize);
+console.log('execute "tail -f %s" to watch log. press ctrl-c to exit.', logfile);
+console.log('Starting:', new Date());
 
-var WebSocket = require('ws');
+const WebSocket = require('ws');
+
 var ws = new WebSocket('wss://ws-sandbox.kraken.com');
-ws.on('open', function() {
+ws.on('open', () => {
 	console.log('Open');
-	let subData = {
-	  "event": "subscribe",
-	  "pair": [
-	    "LTC/USD"
-	  ],
-	  "subscription": {
-	    "name": "ticker"
-	  }
+	const subData = {
+		event: 'subscribe',
+		pair: [
+		'BTC/USD',
+		'ETH/USD',
+		'LTC/USD'
+		],
+		subscription: {
+			name: 'ticker'
+		}
 	};
 
 	ws.send(JSON.stringify(subData));
 });
 
-ws.on('close', function() {
-    console.log('disconnected');
-    logStats();
-	console.log('Rotations: ', rotations);
-	console.log('Finished: ', new Date());
+ws.on('close', () => {
+	console.log('disconnected');
+	logStats();
+	console.log('Rotations:', rotations);
+	console.log('Finished:', new Date());
 	process.exit(0);
 });
 
-ws.on('message', function(message) {
-	console.log('received a message', message);
-	var data;
-	// try {
-	// 	data = JSON.parse(message);
-	// 	log.write(JSON.stringify(data));
-	// } catch(e) {
-	// 	console.log('There was an error parsing JSON', message);
-	// }
+ws.on('message', rawMessage => {
+	console.log('received a message', rawMessage);
+
+	try {
+		const message = JSON.parse(rawMessage);
+		if (Array.isArray(message)) {
+			console.log('got a proper message!');
+			count++;
+			log.write(rawMessage + '\n');
+			// Log.write(data);
+		}
+	} catch (error) {
+
+	}
 });
 
 function logStats() {
-	console.log('Total: ', count);
+	console.log('Total:', count);
 }
 
-setInterval(function() {
+setInterval(() => {
 	logStats();
-}, 1000);
+}, 5000);
